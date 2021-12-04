ecaecf2e7d0aa8298d83480ccb94a0955d297ac9
zhihang <35970192+zhihangfu@users.noreply.github.com>
2021-12-04 12:13:50 +0800

    Add files via upload

---
+++ BlockProjection.py
@@ -0,0 +1,39 @@
+"""
+Project Blocks (vertically) to selected Objects.
+Currently not working with SubD
+"""
+
+
+import rhinoscriptsyntax as rs
+
+
+# ask for inputs
+blocks = rs.GetObjects(message="Select blocks to project", filter=4096, preselect=True, select=True)
+targets = rs.GetObjects(message="Select surfaces, polysurfaces, SubDs and meshes to project onto", filter=8|16|32|1073741824) # not applicable for SubD(262144)
+
+# tweak blocks one by one
+count = 0
+for b in blocks:
+    
+    if rs.IsBlockInstance(b):
+        ptFrom = rs.BlockInstanceInsertPoint(b)
+       
+        # find pt to move to
+        for t in targets:
+            if rs.IsSurface(t) or rs.IsPolysurface(t):
+                ptTo = rs.ProjectPointToSurface(ptFrom,t,[0,0,1])
+            elif rs.IsMesh(t):
+                ptTo = rs.ProjectPointToMesh(ptFrom,t,[0,0,1])
+            
+            if len(ptTo) != 0: break
+        
+        # move block vertically
+        if len(ptTo) != 0:
+            ptTo = ptTo[0]
+            movement = rs.VectorCreate(ptTo, ptFrom)
+            rs.MoveObject(b, movement)
+            count += 1
+
+print "Projected",
+print count,
+print "Blocks to targets."
